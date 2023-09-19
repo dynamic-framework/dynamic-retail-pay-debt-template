890779b6667a291032db14b1596c599bebbf97b9
Johann Felipe Rojas <johannrip@gmail.com>
2023-09-18 20:07:21 -0500

    test: mock react-responsive-pagination

--- jest.config.js
+++ jest.config.js
@@ -9,5 +9,6 @@ module.exports = {
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/tests/__mocks__/fileMock.js',
     '\\.(css|sass)$': '<rootDir>/tests/__mocks__/styleMock.js',
+    '^react-responsive-pagination$': '<rootDir>/tests/__mocks__/reactPagination.js',
   },
 };
