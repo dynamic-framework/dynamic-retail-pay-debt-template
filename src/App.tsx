57be44f3459344f39d15fa1f6f06e6131b2f09dd
Johann Felipe Rojas <johannrip@gmail.com>
2023-10-27 10:30:22 -0500

    chore: remove main title

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { useTranslation } from 'react-i18next';
-
 import { useAppSelector } from './store/hooks';
 import { getIsPaid } from './store/selectors';
 
@@ -10,17 +8,11 @@ import useLoanAccountEffect from './services/hooks/useLoanAccountEffect';
 export default function App() {
   useLoanAccountEffect();
   const isPaid = useAppSelector(getIsPaid);
-  const { t } = useTranslation();
 
   return (
-    <div className="py-3">
-      <h1 className="fs-4 fw-bold mb-3 mb-md-5">
-        {t('title')}
-      </h1>
-      <div className="row justify-content-center">
-        <div className="col-lg-8 col-xl-6">
-          {!isPaid ? <Payment /> : <PaymentResult />}
-        </div>
+    <div className="row justify-content-center py-3">
+      <div className="col-lg-8 col-xl-6">
+        {!isPaid ? <Payment /> : <PaymentResult />}
       </div>
     </div>
   );
