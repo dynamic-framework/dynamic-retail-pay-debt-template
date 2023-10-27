a05bfa776429e0307691ddfc0d774843c115cf45
Johann Felipe Rojas <johannrip@gmail.com>
2023-10-27 10:30:52 -0500

    chore: import base styles and replace context provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import {
-  LiquidContextProvider,
+  DContextProvider,
   ModalContextProvider,
   DToastContainer,
 } from '@dynamic-framework/ui-react';
@@ -17,16 +17,13 @@ import ModalPaymentAlternatives from './components/ModalPaymentAlternatives';
 import ModalConfirmPayment from './components/ModalConfirmPayment';
 import ModalAutoDebt from './components/ModalAutoDebt';
 
-if (process.env.NODE_ENV === 'development') {
-  // eslint-disable-next-line global-require
-  require('@dynamic-framework/ui-react/dist/css/dynamic-ui.css');
-}
-require('./style/base.scss');
+import '@dynamic-framework/ui-react/dist/css/dynamic-ui.css';
+import './styles/base.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('payDebtTemplate') as Element);
 root.render(
   <React.StrictMode>
-    <LiquidContextProvider>
+    <DContextProvider>
       <Provider store={store}>
         <ModalContextProvider
           portalName="modalPortal"
@@ -40,7 +37,7 @@ root.render(
           <DToastContainer />
         </ModalContextProvider>
       </Provider>
-    </LiquidContextProvider>
+    </DContextProvider>
   </React.StrictMode>,
 );
 
@@ -48,4 +45,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
