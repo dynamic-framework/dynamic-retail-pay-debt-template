bae3c4d40ab194008e458ff2c5efa485ecdc6be3
Johann Felipe Rojas <johannrip@gmail.com>
2024-02-27 11:48:11 -0500

    refactor: update client import

--- src/services/repositories/AccountRepository.ts
+++ src/services/repositories/AccountRepository.ts
@@ -1,6 +1,6 @@
 import type { GenericAbortSignal } from 'axios';
 
-import ApiClient from '../clients/ApiClient';
+import ApiClient from '../clients/apiClient';
 import accountMapper from '../mappers/accountMapper';
 
 import type { ApiAccount } from '../api-interface';
