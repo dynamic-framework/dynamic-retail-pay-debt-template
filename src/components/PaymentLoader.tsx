753904e3a073a4cf2763f64fe53035a9168c6e1b
Johann F Rojas <43022731+johannrp27@users.noreply.github.com>
2024-10-02 12:02:49 -0500

    chore: replace DSkeleton component with BT5 loader (#21)
    * chore: import strict mode from react
    
    * chore: replace DSkeleton component with BT5 loader

---
+++ src/components/PaymentLoader.tsx
@@ -0,0 +1,48 @@
+export default function PaymentLoader() {
+  return (
+    <div className="d-flex flex-column align-items-center gap-4 placeholder-glow">
+      <span
+        className="placeholder bg-secondary-200 rounded-2 col-12"
+        style={{
+          height: '5rem',
+        }}
+      />
+      <span
+        className="placeholder bg-secondary-200 rounded-2 col-12 mt-2"
+        style={{
+          height: '6rem',
+        }}
+      />
+      <span
+        className="placeholder bg-secondary-200 rounded-2 col-12 mt-2"
+        style={{
+          height: '5rem',
+        }}
+      />
+      <span
+        className="placeholder bg-secondary-200 rounded-2 col-12"
+        style={{
+          height: '5rem',
+        }}
+      />
+      <span
+        className="placeholder bg-secondary-200 rounded-2 col-12"
+        style={{
+          height: '5rem',
+        }}
+      />
+      <span
+        className="placeholder bg-secondary-200 rounded-2 col-12"
+        style={{
+          height: '5rem',
+        }}
+      />
+      <span
+        className="placeholder bg-secondary-200 rounded-4 col-2"
+        style={{
+          height: '3rem',
+        }}
+      />
+    </div>
+  );
+}
